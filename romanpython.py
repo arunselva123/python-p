11445f869cb79354c19670acaf42262e54a86678
arunselva123 <41563207+arunselva123@users.noreply.github.com>
2018-07-23 12:38:41 +0530

    Create romanpython.py

---
+++ romanpython.py
@@ -0,0 +1,14 @@
+def RomanToInt(year):
+  result = 0
+  boom = []
+  for i in range(len(year)):
+     for letter, value in values
+     if year[i] == letter:
+      boom.append(value)
+    boom.append(0)
+    for i in range(len(year)):
+     if boom[i] >= boom[i+1]:
+      result = result + boom[i]
+     else:
+    result = result - boom[i]
+   return result
